a6f8cd164910b97b878c5d846c133829e29a4bd5
Olivier Lauzon <olauzon@gmail.com>
2009-12-15 18:03:47 -0500

    Whitespace

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,6 +1,6 @@
 # Taken from http://github.com/ludo/to_slug
 class String
-  
+
   # Generate a slug for the string +value+.
   #
   # A slug should consist of numbers (0-9), lowercase letters (a-z) and 
@@ -18,25 +18,25 @@ class String
   def to_slug
     # Perform transliteration to replace non-ascii characters with an ascii
     # character
-    value = self.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n, '').to_s
-    
+    value = mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n, '').to_s
+
     # Remove single quotes from input
     value.gsub!(/[']+/, '')
 
     # Replace any non-word character (\W) with a space
     value.gsub!(/\W+/, ' ')
-    
+
     # Remove any whitespace before and after the string
     value.strip!
-    
+
     # All characters should be downcased
     value.downcase!
-    
+
     # Replace spaces with dashes
     value.gsub!(' ', '-')
-    
+
     # Return the resulting slug
     value
   end
-  
+
 end
